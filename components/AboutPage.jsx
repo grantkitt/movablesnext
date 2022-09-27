ee463cd909741378a70764ec1325780c59604470
grantkitt <38022110+grantkitt@users.noreply.github.com>
2022-09-27 17:19:32 -0500

    cool

--- components/AboutPage.jsx
+++ components/AboutPage.jsx
@@ -1,6 +1,6 @@
 const AboutPage = () => {
   return (
-    <div className="w-96 lg:w-1/3 mx-auto text-center py-20">
+    <div className="w-80 lg:w-1/3 mx-auto text-center py-20 h-screen">
         <h1 className="text-4xl font-bold text-center">Learn about MovablesATX.</h1>
         <p className="text-justify text-xl mt-5">The greatest vintage furnature company that you are potentially ever going to witness. Some used, some brand new, some for sitting, some for eating, pretty much anything that you could possibly think of. Shop MovablesATX #forlyfe</p>
     </div>
